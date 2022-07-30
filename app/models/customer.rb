a4a7ac55104b3a763e313b213ae79276e9f13fb7
h0namyuju <yuanju0h@gmail.com>
2022-07-30 17:42:12 +0000

    [Add]アソシエーション記述

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_items,address,order,dependent: :destroy
+  has_many :items, :through => :cart_items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+
+  has_many :cart_items
+  has_many :order_details
+  has_many :customers, :through => :cart_items
+  has_many :orders, :through => :order_details
+
+  belongs_to :genre
 end
